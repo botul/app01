53539554017b0f136f2bc21656dcecf3c41b622e
botul <pawel.botulinski@gmail.com>
2022-03-05 20:24:10 +0100

    feat: form for submitting files

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+from crypt import methods
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
 
