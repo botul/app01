f85018bcb0ca50a811bac0be12417ffb0fd04f45
botul <pawel.botulinski@gmail.com>
2022-03-05 14:13:40 +0100

    feat: render page by html template

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+def main():
+    return render_template('index.html')
 
 
